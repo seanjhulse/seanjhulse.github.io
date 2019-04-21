ef1c95e0cf3fa5abbbb4fa23007a867aa66c9eaa
Sean Hulse <seanjhulse@gmail.com>
2019-04-21 11:59:38 -0500

    Testing git hooks

---
+++ hooks/pre-commit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+
+def parse_args():
+  pass
+
+def main(args=None):
+  print("Hello, world.")
+  pass
+
+if __name__ == "__main__":
+  args = parse_args()
+  main(args)
