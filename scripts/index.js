dcb419abb28467f51a48fa7cce7bc91db4078722
Sean Hulse <seanjhulse@gmail.com>
2019-04-21 13:48:23 -0500

    Add theming button to the top right of the page

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function() {
+  setTheme();
+  document.getElementById('theme-button').addEventListener('click', setThemeListener);
+});
+
+const setThemeListener = function() {
+  let theme = localStorage.getItem('theme');
+  if(theme) {
+    if(theme == 'dark') {
+      localStorage.setItem('theme', 'light');
+    } else {
+      localStorage.setItem('theme', 'dark');
+    }
+  } else {
+    // default is dark so swap if not set
+    localStorage.setItem('theme', 'light');
+  }
+
+  setTheme();
+}
+const setTheme = function() {
+  let theme = localStorage.getItem('theme');
+  if(theme == "dark") {
+    document.getElementById('theme-button').innerText = "Dark Theme";
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+  }
+  if(theme == "light") {
+    document.getElementById('theme-button').innerText = "Light Theme";
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+  }
+}
